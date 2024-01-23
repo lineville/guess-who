4f92b29caacc3d1758110608aa20e6fe747e6326
Liam Neville <lineville@github.com>
2024-01-22 20:39:15 -0500

    fix

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import Home from '../pages/Home';
-
-function MyApp({ Component = Home, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
