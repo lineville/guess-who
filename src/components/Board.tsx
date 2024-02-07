703b991a32641193b24b8d99bbc7aae9ea113580
Liam Neville <lineville@github.com>
2024-02-07 14:57:51 -0500

    revert to nextjs image

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,23 +1,26 @@
 import Character from "@/character";
 import styles from '../styles.module.css'
-import { Image, Card, Text, Grid, GridItem, CardBody, CardFooter, Box, useColorMode } from "@chakra-ui/react";
+import Image from 'next/image'
+import { Card, Text, Grid, GridItem, CardBody, CardFooter, Box, useColorMode } from "@chakra-ui/react";
 
 export default function Board({ board, handleClickCharacter, columns }: { board: Character[], handleClickCharacter: (index: number) => void, columns: number }) {
   const { colorMode } = useColorMode();
-
+// TODO scroll on AI question modal on mobile
+// TODO store flipped over chars on server
+// 
   return (
     <Grid templateColumns={`repeat(${columns}, 1fr)`} gap={3}>
       {board.map((c, index) => (
         <GridItem key={index} onClick={() => handleClickCharacter(index)}>
           <Box>
-            <Card className={colorMode === 'light' ? styles.imageHoverLight : styles.imageHoverDark} display="flex" justifyContent="center" alignItems="center" w="100%" h="100%">
+            <Card className={colorMode === 'light' ? styles.imageHoverLight : styles.imageHoverDark} display="flex" justifyContent="center" alignItems="center" w={['120', '150']} h={['150', '180']}>
               <CardBody p={2}>
                 <Box>
-                  <Image src={c.alive ? `/${c.name}.png` : "/question-mark.png"} alt={c.name} fallbackSrc={"/question-mark.png"} w={['100', '130']} h={['100', '130']} objectFit="cover" />
+                  <Image src={c.alive ? `/${c.name}.png` : "/question-mark.png"} alt={c.name} placeholder="empty" priority={true} sizes="(max-width: 768px) 100px, 130px" width={100} height={100} objectFit="cover"/>
                 </Box>
               </CardBody>
-              <CardFooter display="flex" alignItems="center" justifyContent="center">
-                <Text fontSize={['12px', '20px']}>{c.name}</Text>
+              <CardFooter display="flex" alignItems="center" justifyContent="center" w="100%" h="100%">
+                <Text fontSize={['10px', '20px']} maxW={['100', '300']}>{c.name}</Text>
               </CardFooter>
             </Card>
           </Box>
