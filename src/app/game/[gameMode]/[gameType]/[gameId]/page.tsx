456874042a6fcf728c7803c86a6fe5a59b5bde5e
Liam Neville <lineville@github.com>
2024-04-04 10:24:34 -0400

    fix build

--- src/app/game/[gameMode]/[gameType]/[gameId]/page.tsx
+++ src/app/game/[gameMode]/[gameType]/[gameId]/page.tsx
@@ -3,11 +3,9 @@
 import Game from "@/components/Game";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { GameMode } from "@/lib/gameMode";
 
 export default function Page() {
   const [clientId, setClientId] = useState<string>("");
-  const [gameMode, setGameMode] = useState<GameMode>(GameMode.MultiPlayer);
 
   useEffect(() => {
     const clientId = localStorage.getItem("clientId");
@@ -15,14 +13,7 @@ export default function Page() {
       localStorage.setItem("clientId", uuidv4());
     }
     setClientId(localStorage.getItem("clientId") as string);
-
-    // Determine game mode based on URL query parameters
-    const urlParams = new URLSearchParams(window.location.search);
-    const mode = urlParams.get('mode');
-    if (mode === GameMode.SinglePlayer) {
-      setGameMode(GameMode.SinglePlayer);
-    }
   }, []);
 
-  return clientId && <Game clientId={clientId} gameMode={gameMode} />;
+  return clientId && <Game clientId={clientId} />;
 }
