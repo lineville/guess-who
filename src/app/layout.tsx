3056a4abe8a6c9b56266fc55f45a1377abb28c8b
Liam Neville <lineville@github.com>
2025-01-05 14:43:58 -0500

    pwa with kiki favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Guess Who" />
       </head>
       <body
         style={{
